70e291ad569a91077e21e94df8c465417c42feac
Robert Anderson <rcanderson23@gmail.com>
2018-07-16 09:37:40 -0600

    moved to ldaps and python3

--- ldap-ad.py
+++ ldap-ad.py
@@ -1,11 +1,11 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 
 import os
 import sys
 import re
-import ldap
+import ldap3
 import json
-import ConfigParser
+import configparser
 import argparse
 
 parser = argparse.ArgumentParser(description='Script to obtain host inventory from AD')
@@ -17,41 +17,45 @@ class ADAnsibleInventory():
     def __init__(self):
         directory = os.path.dirname(os.path.abspath(__file__))
         configfile = directory + '/ldap-ad.ini' 
-        config = ConfigParser.ConfigParser()
+        config = configparser.ConfigParser()
         config.read(configfile)
         domaindn = config.get('ldap-ad', 'domaindn')
         username = config.get('ldap-ad', 'username')
         password = config.get('ldap-ad', 'password')
         basedn = config.get('ldap-ad', 'basedn')
         ldapuri = config.get('ldap-ad', 'ldapuri') 
-        scope = ldap.SCOPE_SUBTREE
+        port = config.get('ldap-ad', 'port')
+        ca_file = config.get('ldap-ad', 'ca_file')
         adfilter = "(&(sAMAccountType=805306369))"
         
         self.inventory = {}
-        self.ad_connect(ldapuri, username, password)
-        self.get_hosts(basedn,scope,adfilter)
+        self.ad_connect(ldapuri, username, password, port, ca_file)
+        self.get_hosts(basedn,adfilter)
         self.org_hosts(basedn)
-        print json.dumps(self.inventory, indent=2, encoding='iso-8859-9')
+        print(json.dumps(self.inventory, indent=2))
         
-    def ad_connect(self, ldapuri, username, password):
-        conn = ldap.initialize(ldapuri)
-        conn.protocol_version = ldap.VERSION3
-        conn.set_option(ldap.OPT_REFERRALS, 0)
-        conn.simple_bind_s(username, password)
+    def ad_connect(self, ldapuri, username, password, port, ca_file):
+        tls_configuration = ldap3.Tls(ca_certs_path=ca_file)
+        conn = ldap3.Connection(ldapuri, 
+                                auto_bind=True, 
+                                user=username, 
+                                password=password, 
+                                authentication=ldap3.NTLM)
         self.conn = conn
-    
-    def get_hosts(self, basedn, scope, adfilter):
-        self.results = self.conn.search_s(basedn, scope, adfilter)
-   
-    def get_groups(self, basedn, scope, adfilter):
-        self.groups = self.conn.search_s(basedn, scope, adfilter)
 
+    def get_hosts(self, basedn, adfilter):
+        self.conn.search(search_base=basedn,
+                         search_filter=adfilter,
+                         attributes=['cn','dnshostname'])
+        self.conn.response_to_json
+        self.results = self.conn.response
+   
     def org_hosts(self, basedn):
 
         #Removes CN,OU, and DC and places into a list
         basedn_list = (re.sub(r"..=","", basedn)).split(",")
-        for dn,attrs in self.results:
-            org_list = (re.sub(r"..=","",dn)).split(",")
+        for computer in self.results:
+            org_list = (re.sub(r"..=","",computer['dn'])).split(",")
             #Remove hostname
             del org_list[0]
             
@@ -65,7 +69,7 @@ class ADAnsibleInventory():
             org_range = range(0,(len(org_list)))
             for orgs in org_range:
                 if orgs == org_range[-1]:
-                    self.add_host(org_list[orgs], attrs['dNSHostName'])
+                    self.add_host(org_list[orgs], computer['attributes']['dNSHostName'])
                 else:
                     self.add_group(group=org_list[orgs], children=org_list[orgs+1])
     def add_host(self, group, host):
